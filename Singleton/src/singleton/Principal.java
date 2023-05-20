23e48859c0cf26013614efdb3eaae83c3fd2901d
Bárbara Wiest <116441019+barbaracw@users.noreply.github.com>
2023-05-20 00:14:50 -0300

    Add files via upload

---
+++ Singleton/src/singleton/Principal.java
@@ -0,0 +1,18 @@
+package singleton;
+
+public class Principal {
+
+    public static void main(String[] args) {
+
+        //Construtor ilegal
+        //SingleObject object = new SingleObject(); 
+        
+        //Recupera um objeto
+        SingleObject object1 = SingleObject.getInstance();
+        object1.showMessage();
+
+        SingleObject object2 = SingleObject.getInstance();
+        object2.showMessage();
+    }
+
+}
